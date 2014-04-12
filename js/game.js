5e6e685561c795b20db3af502fc540c86e82af1f
Niko <niksancho96@gmail.com>
2014-04-13 01:11:44 +0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,24 +1,32 @@
 $(document).ready(function() {
-    infoBoxes();
-    init();
-    
+    var playerName;
     var cpuChoice = genNumber();
     var userChoice;
+    var userWins = 0;
+    var userLose = 0;
+    var ties = 0;
+    
+    infoBoxes();
+    init();
+    displayResult(playerName, userWins, userLose, ties);
     
     document.getElementById('rock').addEventListener('click', function(){
         userChoice = "Rock";
         if (compare(cpuChoice, userChoice) == "cpuWin") {
             $("#winner-text").text("You lost, the computer won with " + cpuChoice);
+            userLose += 1;
+            displayResult(userWins, userLose, ties);
             cpuChoice = genNumber();
-            console.log(cpuChoice);
         } else if (compare(cpuChoice, userChoice) == "userWin") {
             $("#winner-text").text("You won, the computer lost with " + cpuChoice);
+            userWins += 1;
+            displayResult(userWins, userLose, ties);
             cpuChoice = genNumber();
-            console.log(cpuChoice);
         } else {
             $("#winner-text").text("It's a tie! You both choosed the " + cpuChoice);
+            ties += 1;
+            displayResult(userWins, userLose, ties);
             cpuChoice = genNumber();
-            console.log(cpuChoice);
         }
     });
     
@@ -26,16 +34,19 @@ $(document).ready(function() {
         userChoice = "Paper";
         if (compare(cpuChoice, userChoice) == "cpuWin") {
             $("#winner-text").text("You lost, the computer won with " + cpuChoice);
+            userLose += 1;
+            displayResult(userWins, userLose, ties);
             cpuChoice = genNumber();
-            console.log(cpuChoice);
         } else if (compare(cpuChoice, userChoice) == "userWin") {
             $("#winner-text").text("You won, the computer lost with " + cpuChoice);
+            userWins += 1;
+            displayResult(userWins, userLose, ties);
             cpuChoice = genNumber();
-            console.log(cpuChoice);
         } else {
             $("#winner-text").text("It's a tie! You both choosed the " + cpuChoice);
+            ties += 1;
+            displayResult(userWins, userLose, ties);
             cpuChoice = genNumber();
-            console.log(cpuChoice);
         }
     });
     
@@ -43,22 +54,25 @@ $(document).ready(function() {
         userChoice = "Scissors";
         if (compare(cpuChoice, userChoice) == "cpuWin") {
             $("#winner-text").text("You lost, the computer won with " + cpuChoice);
+            userLose += 1;
+            displayResult(userWins, userLose, ties);
             cpuChoice = genNumber();
-            console.log(cpuChoice);
         } else if (compare(cpuChoice, userChoice) == "userWin") {
             $("#winner-text").text("You won, the computer lost with " + cpuChoice);
+            userWins += 1;
+            displayResult(userWins, userLose, ties);
             cpuChoice = genNumber();
-            console.log(cpuChoice);
         } else {
             $("#winner-text").text("It's a tie! You both choosed the " + cpuChoice);
+            ties += 1;
+            displayResult(userWins, userLose, ties);
             cpuChoice = genNumber();
-            console.log(cpuChoice);
         }
     });
 });
 
 function init() {
-    var playerName = prompt("Please enter your name");
+    playerName = prompt("Please enter your name");
     playerName = setPlayerName(playerName);
     $("#greet-msg").text("Hello, " + playerName + "! This is the game Rock, paper, scissor! You know the rules, so rock on! Get it? :D");
 }
@@ -118,4 +132,8 @@ function setPlayerName(name) {
     } else {
         return name;
     }
+}
+
+function displayResult(wins, lose, tie) {
+    $("#info-game-stats").text("Current player is: " + playerName + ", who has won: " + wins + ", has lost: " + lose + " and has tied with the bot: " + tie + " times!");
 }
